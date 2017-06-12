342ff39c5831f3c0a00e36551bbd96c452e56e62
Sarah Radford <sarah.radford@ons.gov.uk>
2017-06-12 10:06:03 +0000

    CTPA-1323 Foundation schema and seed data

--- actionexportersvc/src/main/resources/database/changes/release-9.33.1/actionexport_2017_actionplan_null.sql
+++
@@ -1,5 +0,0 @@
---Remove null constraint from actionplan column, translation requests do not run as part of an actionplan
-
-set schema 'actionexporter';
-
-ALTER TABLE actionrequest ALTER COLUMN actionplan DROP NOT NULL;
