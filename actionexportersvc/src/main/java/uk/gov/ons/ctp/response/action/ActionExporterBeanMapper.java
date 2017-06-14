d644283440145a8211a36bc20624baf74f3fae5d
Edward Stevens <edward.stevens@ons.gov.uk>
2017-06-14 13:08:05 +0100

    CTPA-1305 Added Orika StringToUUIDConverter

--- actionexportersvc/src/main/java/uk/gov/ons/ctp/response/action/ActionExporterBeanMapper.java
+++ actionexportersvc/src/main/java/uk/gov/ons/ctp/response/action/ActionExporterBeanMapper.java
@@ -1,10 +1,12 @@
 package uk.gov.ons.ctp.response.action;
 
 import ma.glasnost.orika.MapperFactory;
+import ma.glasnost.orika.converter.ConverterFactory;
 import ma.glasnost.orika.impl.ConfigurableMapper;
 import ma.glasnost.orika.impl.DefaultMapperFactory;
 import ma.glasnost.orika.impl.generator.EclipseJdtCompilerStrategy;
 import org.springframework.stereotype.Component;
+import uk.gov.ons.ctp.common.util.StringToUUIDConverter;
 import uk.gov.ons.ctp.response.action.export.domain.ActionRequestInstruction;
 import uk.gov.ons.ctp.response.action.export.domain.TemplateExpression;
 import uk.gov.ons.ctp.response.action.export.domain.TemplateMapping;
@@ -30,6 +32,10 @@ public class ActionExporterBeanMapper extends ConfigurableMapper {
    */
   @Override
   protected final void configure(final MapperFactory factory) {
+
+    ConverterFactory converterFactory = factory.getConverterFactory();
+    converterFactory.registerConverter(new StringToUUIDConverter());
+
     factory
             .classMap(TemplateExpression.class, TemplateExpressionDTO.class)
             .byDefault()
