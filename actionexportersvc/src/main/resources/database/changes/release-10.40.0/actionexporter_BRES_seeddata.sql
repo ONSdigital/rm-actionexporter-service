7a32ffb7f4b639faa64c99493ebfad01f31e4f7d
Chris Hardman <christopher.hardman@ons.gov.uk>
2017-06-21 14:44:21 +0100

    CTPA-1336: changed seed data

--- actionexportersvc/src/main/resources/database/changes/release-10.40.0/actionexporter_BRES_seeddata.sql
+++ actionexportersvc/src/main/resources/database/changes/release-10.40.0/actionexporter_BRES_seeddata.sql
@@ -2,7 +2,9 @@
 set schema 'actionexporter';
 
 INSERT INTO template (templatenamePK,content,datemodified)
-VALUES ('initialPrint','<#list actionRequests as actionRequest>${(actionRequest.address.sampleunitrefpk)!}|${actionRequest.iac?trim}|${(actionRequest.contact.forename)!}|${(actionRequest.contact.surname)!}</#list>'  , now());
+VALUES ('initialPrint','<#list actionRequests as actionRequest>
+  ${(actionRequest.address.sampleunitrefpk)!}|${actionRequest.iac?trim}|${(actionRequest.contact.forename)!}|${(actionRequest.contact.surname)!}
+  </#list>'  , now());
 
 INSERT INTO templatemapping (actiontypenamePK,templatenameFK,file,datemodified) VALUES ('BRESEL' , 'initialPrint', 'BRESEL', now());
 INSERT INTO templatemapping (actiontypenamePK,templatenameFK,file,datemodified) VALUES ('BRESERL', 'initialPrint', 'BRESERL', now());
