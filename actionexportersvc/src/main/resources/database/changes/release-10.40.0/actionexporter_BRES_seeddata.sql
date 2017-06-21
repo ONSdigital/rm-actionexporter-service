1c3dd9c9998be00103ed3e25440dbbdf938fa841
Chris Hardman <christopher.hardman@ons.gov.uk>
2017-06-21 12:36:50 +0100

    CTPA-1336: changed foudation schema with new template for Bres

--- actionexportersvc/src/main/resources/database/changes/release-10.40.0/actionexporter_BRES_seeddata.sql
+++ actionexportersvc/src/main/resources/database/changes/release-10.40.0/actionexporter_BRES_seeddata.sql
@@ -2,11 +2,7 @@
 set schema 'actionexporter';
 
 INSERT INTO template (templatenamePK,content,datemodified)
-VALUES ('initialPrint','<#list actionRequests as actionRequest>
-${actionRequest.iac?trim}|${(actionRequest.caseRef)!}|${(actionRequest.contact.title)!}|${(actionRequest.contact.forename)!}|${(actionRequest.contact.surname)!}|${(actionRequest.address.organisationName)!}|${(actionRequest.address.line1)!}|${(actionRequest.address.line2)!}|${(actionRequest.address.locality)!}|${(actionRequest.address.townName)!}|${(actionRequest.address.postcode)!}
-</#list>'  , now());
+VALUES ('initialPrint','<#list actionRequests as actionRequest>${(actionRequest.address.sampleunitrefpk)!}|${actionRequest.iac?trim}|${(actionRequest.contact.forename)!}|${(actionRequest.contact.surname)!}</#list>'  , now());
 
 INSERT INTO templatemapping (actiontypenamePK,templatenameFK,file,datemodified) VALUES ('BRESEL' , 'initialPrint', 'BRESEL', now());
 INSERT INTO templatemapping (actiontypenamePK,templatenameFK,file,datemodified) VALUES ('BRESERL', 'initialPrint', 'BRESERL', now());
-
-
