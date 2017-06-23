202765b1749b22a460ed9058ab049169c40acb44
Chris Hardman <christopher.hardman@ons.gov.uk>
2017-06-23 09:31:08 +0100

    CTPA-1336: added to print file template

--- actionexportersvc/src/main/resources/database/changes/release-10.40.0/actionexporter_BRES_seeddata.sql
+++ actionexportersvc/src/main/resources/database/changes/release-10.40.0/actionexporter_BRES_seeddata.sql
@@ -3,7 +3,7 @@ set schema 'actionexporter';
 
 INSERT INTO template (templatenamePK,content,datemodified)
 VALUES ('initialPrint','<#list actionRequests as actionRequest>
-  ${(actionRequest.address.sampleunitrefpk)!}|${actionRequest.iac?trim}|${(actionRequest.contact.forename)!}|${(actionRequest.contact.surname)!}
+  ${(actionRequest.address.sampleunitrefpk)!}|${actionRequest.iac?trim}|${(actionRequest.contact.forename)!}|${(actionRequest.contact.surname)!|${(actionRequest.contact.emailaddress)!}
   </#list>'  , now());
 
 INSERT INTO templatemapping (actiontypenamePK,templatenameFK,file,datemodified) VALUES ('BRESEL' , 'initialPrint', 'BRESEL', now());
