1856401828b8c7878ba81094343423f557af5d05
Chris Hardman <christopher.hardman@ons.gov.uk>
2017-07-20 08:33:17 +0100

    CTPA-1451: added trim syntax to seed data within template

--- actionexportersvc/src/main/resources/database/changes/release-10.43.0/update_template_contents.sql
+++ actionexportersvc/src/main/resources/database/changes/release-10.43.0/update_template_contents.sql
@@ -1,4 +1,4 @@
 UPDATE actionexporter.template
 SET content = '<#list actionRequests as actionRequest>
-  ${(actionRequest.address.sampleUnitRef)!}:${actionRequest.iac?trim}:${(actionRequest.contact.forename?trim)!}:${(actionRequest.contact.emailaddress)!}
+  ${(actionRequest.address.sampleUnitRef?trim)!}:${actionRequest.iac?trim}:${(actionRequest.contact.forename?trim)!}:${(actionRequest.contact.emailaddress)!}
   </#list>';
