3f0eb70d2271139d2099f8d1dd0666c628793a6c
Richard Weeks <richard.weeks@ons.gov.uk>
2018-01-30 14:50:56 +0000

    Use Lombok for getter and setter.

--- src/main/java/uk/gov/ons/ctp/response/action/export/domain/SurveyRefExerciseRef.java
+++ src/main/java/uk/gov/ons/ctp/response/action/export/domain/SurveyRefExerciseRef.java
@@ -1,30 +1,16 @@
 package uk.gov.ons.ctp.response.action.export.domain;
 
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import lombok.Data;
+
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class SurveyRefExerciseRef {
 
         private String surveyRef;
         private String exerciseRef;
-
-    public SurveyRefExerciseRef(String surveyRef, String exerciseRef) {
-        this.surveyRef = surveyRef;
-        this.exerciseRef = exerciseRef;
-    }
-
-    public void setSurveyRef(String surveyRef) {
-        this.surveyRef = surveyRef;
-    }
-
-    public void setExerciseRef(String exerciseRef) {
-        this.exerciseRef = exerciseRef;
-    }
-
-    public String getSurveyRef() {
-
-        return surveyRef;
-    }
-
-    public String getExerciseRef() {
-        return exerciseRef;
-    }
 }
 
