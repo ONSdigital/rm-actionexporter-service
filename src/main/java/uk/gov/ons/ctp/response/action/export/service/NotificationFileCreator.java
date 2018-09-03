4ea2460465d518e3ba3484435310ff9083b2ab54
Ben Jefferies <benjefferies@echosoft.uk>
2018-09-03 11:59:19 +0100

    Fix nested if

--- src/main/java/uk/gov/ons/ctp/response/action/export/service/NotificationFileCreator.java
+++ src/main/java/uk/gov/ons/ctp/response/action/export/service/NotificationFileCreator.java
@@ -78,9 +78,7 @@ public class NotificationFileCreator {
     final String now = FILENAME_DATE_FORMAT.format(clock.millis());
     String filename = String.format("%s_%s.csv", filenamePrefix, now);
     if (exportData.isEmpty()) {
-      if (exportData.isEmpty()) {
-        log.with("filename", filename).info("No data to generate file");
-      }
+      log.with("filename", filename).info("No data to generate file");
       return;
     }
     sftpService.sendMessage(
