646ca44f61308d7088732f0c09a89c7fb742f7ce
damien-lloyd <damien.lloyd21@gmail.com>
2018-09-07 09:01:46 +0100

    Merge conflicts

--- src/test/java/uk/gov/ons/ctp/response/action/export/service/ActionInstructionReceiverIT.java
+++ src/test/java/uk/gov/ons/ctp/response/action/export/service/ActionInstructionReceiverIT.java
@@ -1,14 +1,9 @@
 package uk.gov.ons.ctp.response.action.export.service;
 
-import static com.github.tomakehurst.wiremock.core.WireMockConfiguration.options;
 import static org.mockito.Matchers.any;
 import static org.mockito.Mockito.doThrow;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
-
-import com.github.tomakehurst.wiremock.extension.responsetemplating.ResponseTemplateTransformer;
-import com.github.tomakehurst.wiremock.junit.WireMockRule;
-import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,7 +12,6 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.messaging.Message;
 import org.springframework.messaging.MessageChannel;
-import org.springframework.messaging.MessageHandlingException;
 import org.springframework.messaging.support.GenericMessage;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.ContextConfiguration;
@@ -37,12 +31,8 @@ public class ActionInstructionReceiverIT {
 
   @MockBean private ActionExportService actionExportService;
 
-  @Rule
-  public WireMockRule wireMockRule =
-      new WireMockRule(options().extensions(new ResponseTemplateTransformer(false)).port(18002));
-
-  @Test(expected = MessageHandlingException.class)
-  public void ensureFiniteRetriesOnFailedActionInstruction() throws Exception {
+  @Test
+  public void ensureFiniteRetriesOnFailedActionInstruction() {
     doThrow(RuntimeException.class).when(actionExportService).acceptInstruction(any());
 
     ActionRequest actionRequest = ActionRequestBuilder.createSocialActionRequest("SOCIALPRENOT");
