b837fd080f900c837fd0deb8ef5347320da8d0b9
AdamHawtin <adam.hawtin@ons.gov.uk>
2018-11-09 08:59:24 +0000

    Drop old index and create new one on foreign key, formatting changes

--- src/main/resources/database/changes/release-16/use_integer_pk_in_address_and_add_fk_to_action_request.sql
+++ src/main/resources/database/changes/release-16/use_integer_pk_in_address_and_add_fk_to_action_request.sql
@@ -5,18 +5,22 @@ CREATE SEQUENCE actionexporter.addressPKseq
     MAXVALUE 999999999999
     CACHE 1;
 
-ALTER TABLE actionexporter.address ADD COLUMN addressPK bigint DEFAULT nextval('actionexporter.addressPKseq') NOT NULL;
+ALTER TABLE ONLY actionexporter.address ADD COLUMN addressPK bigint DEFAULT nextval('actionexporter.addressPKseq') UNIQUE NOT NULL;
 
-ALTER TABLE actionexporter.actionrequest ADD COLUMN addressFK bigint;
+ALTER TABLE ONLY actionexporter.actionrequest ADD COLUMN addressFK bigint;
 
 UPDATE actionexporter.actionrequest AS ar
-SET addressfk = a.addresspk
+SET addressFK = a.addressPK
 FROM actionexporter.address AS a
-WHERE ar.sampleunitreffk = a.sampleunitrefpk;
+WHERE ar.sampleunitrefFK = a.sampleunitrefPK;
 
 ALTER TABLE ONLY actionexporter.actionrequest DROP CONSTRAINT sampleunitrefFK_fkey;
 ALTER TABLE ONLY actionexporter.actionrequest RENAME COLUMN sampleunitrefFK TO sampleunitref;
 ALTER TABLE ONLY actionexporter.address DROP CONSTRAINT sampleunitrefPK_pkey;
 ALTER TABLE ONLY actionexporter.address ADD CONSTRAINT addressPK_pkey PRIMARY KEY (addressPK);
+ALTER TABLE ONLY actionexporter.address DROP CONSTRAINT IF EXISTS address_addressPK_key;
 ALTER TABLE ONLY actionexporter.actionrequest ADD CONSTRAINT addressFK_fkey FOREIGN KEY (addressFK) REFERENCES actionexporter.address(addressPK);
 ALTER TABLE ONLY actionexporter.address DROP COLUMN sampleunitrefPK;
+
+DROP INDEX actionexporter.actionrequest_sampleunitrefFK_index;
+CREATE INDEX actionrequest_addressFK_index on actionexporter.actionrequest(addressFK);
