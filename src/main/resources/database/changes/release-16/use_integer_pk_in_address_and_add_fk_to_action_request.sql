fa2d5829fb90707eaa06e07b6d76b4e2c02e371d
AdamHawtin <adam.hawtin@ons.gov.uk>
2018-11-12 10:46:51 +0000

    Move data migration out of liquibase

--- src/main/resources/database/changes/release-16/use_integer_pk_in_address_and_add_fk_to_action_request.sql
+++ src/main/resources/database/changes/release-16/use_integer_pk_in_address_and_add_fk_to_action_request.sql
@@ -6,13 +6,7 @@ CREATE SEQUENCE actionexporter.addressPKseq
     CACHE 1;
 
 ALTER TABLE ONLY actionexporter.address ADD COLUMN addressPK bigint DEFAULT nextval('actionexporter.addressPKseq') UNIQUE NOT NULL;
-
 ALTER TABLE ONLY actionexporter.actionrequest ADD COLUMN addressFK bigint;
-
-UPDATE actionexporter.actionrequest AS ar
-SET addressFK = a.addressPK
-FROM actionexporter.address AS a
-WHERE ar.sampleunitrefFK = a.sampleunitrefPK;
 
 ALTER TABLE ONLY actionexporter.actionrequest DROP CONSTRAINT sampleunitrefFK_fkey;
 ALTER TABLE ONLY actionexporter.actionrequest RENAME COLUMN sampleunitrefFK TO sampleunitref;
@@ -20,7 +14,8 @@ ALTER TABLE ONLY actionexporter.address DROP CONSTRAINT sampleunitrefPK_pkey;
 ALTER TABLE ONLY actionexporter.address ADD CONSTRAINT addressPK_pkey PRIMARY KEY (addressPK);
 ALTER TABLE ONLY actionexporter.address DROP CONSTRAINT IF EXISTS address_addressPK_key;
 ALTER TABLE ONLY actionexporter.actionrequest ADD CONSTRAINT addressFK_fkey FOREIGN KEY (addressFK) REFERENCES actionexporter.address(addressPK);
-ALTER TABLE ONLY actionexporter.address DROP COLUMN sampleunitrefPK;
+ALTER TABLE ONLY actionexporter.address RENAME COLUMN sampleunitrefPK TO sampleunitref;
+ALTER TABLE ONLY actionexporter.address ALTER COLUMN sampleunitref DROP NOT NULL;
 
 DROP INDEX actionexporter.actionrequest_sampleunitrefFK_index;
 CREATE INDEX actionrequest_addressFK_index on actionexporter.actionrequest(addressFK);
