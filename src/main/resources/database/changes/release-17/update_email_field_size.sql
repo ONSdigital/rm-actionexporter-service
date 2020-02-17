bd06ee2b0e4ac6c62aec1ef9050cb0ef7118e588
Warren Bailey <warren@warrenbailey.net>
2020-02-17 09:20:01 +0000

    Fix liquibase script for updating email lenght (#80)

--- src/main/resources/database/changes/release-17/update_email_field_size.sql
+++ src/main/resources/database/changes/release-17/update_email_field_size.sql
@@ -1,2 +1,2 @@
 -- Change email field length to 200
-ALTER TABLE ONLY actionexporter.contact ALTER COLUMN emailaddress varchar(200);
+ALTER TABLE ONLY actionexporter.contact ALTER COLUMN emailaddress TYPE varchar (200);
