42089c205a5af4b6edac0d42309fe5ff485c6445
Adam Mann <manny160389@hotmail.com>
2020-09-11 13:58:18 +0100

    Fix indentation

--- src/main/java/uk/gov/ons/ctp/response/action/export/endpoint/DeleteEndpoint.java
+++ src/main/java/uk/gov/ons/ctp/response/action/export/endpoint/DeleteEndpoint.java
@@ -1,5 +1,9 @@
 package uk.gov.ons.ctp.response.action.export.endpoint;
 
+import java.sql.Timestamp;
+import java.util.Date;
+import java.util.List;
+import java.util.stream.Stream;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,17 +14,8 @@ import org.springframework.web.bind.annotation.RestController;
 import uk.gov.ons.ctp.common.error.CTPException;
 import uk.gov.ons.ctp.response.action.export.domain.ActionRequestInstruction;
 import uk.gov.ons.ctp.response.action.export.domain.ExportFile;
-import uk.gov.ons.ctp.response.action.export.domain.TemplateMapping;
 import uk.gov.ons.ctp.response.action.export.repository.ActionRequestRepository;
 import uk.gov.ons.ctp.response.action.export.repository.ExportFileRepository;
-import uk.gov.ons.ctp.response.action.export.scheduled.ExportProcessor;
-import uk.gov.ons.ctp.response.action.export.scheduled.ExportScheduler;
-
-import java.sql.Time;
-import java.sql.Timestamp;
-import java.util.Date;
-import java.util.List;
-import java.util.stream.Stream;
 
 @RestController
 @RequestMapping(value = "/delete", produces = "application/json")
@@ -35,19 +30,19 @@ public class DeleteEndpoint {
   public ResponseEntity<String> triggerDelete() throws CTPException {
     try {
       List<ExportFile> exportFiles = exportFileRepository.findAll();
-      for (ExportFile exportFile: exportFiles) {
+      for (ExportFile exportFile : exportFiles) {
         Timestamp dateSuccessfullySent = exportFile.getDateSuccessfullySent();
         long DAY_IN_MS = 1000 * 60 * 60 * 24;
         Date ninetyDaysAgo = new Date(System.currentTimeMillis() - (90 * DAY_IN_MS));
 
         if (dateSuccessfullySent != null && dateSuccessfullySent.before(ninetyDaysAgo)) {
-          Stream<ActionRequestInstruction> actionRequestInstructions = actionRequestRepository.findByExportJobId(
-                  exportFile.getExportJobId());
+          Stream<ActionRequestInstruction> actionRequestInstructions =
+              actionRequestRepository.findByExportJobId(exportFile.getExportJobId());
 
           actionRequestInstructions.forEach(
-            ari -> {
-              actionRequestRepository.delete(ari);
-            });
+              ari -> {
+                actionRequestRepository.delete(ari);
+              });
           exportFileRepository.delete(exportFile);
         }
       }
@@ -57,7 +52,7 @@ public class DeleteEndpoint {
       log.error(
           "Uncaught exception - transaction rolled back. Will re-run when scheduled by cron", e);
       throw new CTPException(
-          CTPException.Fault.SYSTEM_ERROR, "Uncaught exception when exporting print file");
+          CTPException.Fault.SYSTEM_ERROR, "Uncaught exception when deleting old records");
     }
   }
 }
